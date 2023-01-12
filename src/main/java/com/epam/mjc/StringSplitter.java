ccd95d9d58eabe4f5abf2ec0da26a220f5c893af
zhargal <bzhargal@gmail.com>
2023-01-12 08:26:51 +0300

    done

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,6 +1,8 @@
 package com.epam.mjc;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class StringSplitter {
 
@@ -14,10 +16,23 @@ public class StringSplitter {
 
 
     public static List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        ArrayList<String> result = new ArrayList<>();
-        ArrayList<String> arrDelimiters = (ArrayList<String>) delimiters;
+        String[] delimArr = delimiters.toArray(new String[0]);
+        List<String> res = List.of(source.split(delimArr[0]));
 
-        return result;
+        for (int i = 1; i < delimArr.length; i++) {
+            List<String> arr = new ArrayList<>();
+            for (String re : res) {
+                List<String> resSplit = List.of(re.split(delimArr[i]));
+                for (String resSplitEl : resSplit) {
+                    if (!resSplitEl.equals("")) {
+                        arr.add(resSplitEl);
+                    }
+                }
+            }
+
+            res = arr;
+        }
+        return res;
     }
 
 }
